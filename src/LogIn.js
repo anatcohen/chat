6c3ba5379f4bb01b9a5ab28f2ee36eb6e2721669
Anat <anatco13@gmail.com>
2020-03-22 14:28:19 +0200

    added login sass

--- src/LogIn.js
+++ src/LogIn.js
@@ -6,10 +6,14 @@ export default function LogIn(props) {
     const onTextChange = e => {
         let name = e.currentTarget.value, bIsNameUnique = !props.users.users.filter(user => user.name === name).length;
         // Checks whether or not to to display enter button- If text is empty&name is unique
-        document.getElementById('enter').style.visibility = (name.length && bIsNameUnique) ? 'visible' : 'hidden';
+        document.getElementById('enterBtn').style.visibility = (name.length && bIsNameUnique) ? 'visible' : 'hidden';
         // Checks whether or not to display name error- If name is already taken
         document.getElementById('nameAlert').style.visibility = !bIsNameUnique ? 'visible' : 'hidden';
-    };
+    },
+        // On enter button click
+        onEnterClick = e => {
+            props.addSelfToDataBase(document.getElementById('name').value)
+        };
 
 
     useEffect(() => {
@@ -25,9 +29,11 @@ export default function LogIn(props) {
             <div id='rightPanel'>
                 <form autoComplete='off'>
                     <h2>Enter Chat Room:</h2>
-                    <input onChange={onTextChange} type='text' id='name' placeholder='Enter nickname' />
-                    <p id='nameAlert' style={{ visibility: 'hidden' }}>Username is already taken</p>
-                    <Link to='/ChatRoom'><button id='enter' onClick={() => props.addSelfToDataBase(document.getElementById('name').value)} style={{ visibility: 'hidden' }}>Enter</button></Link>
+                    <div className='textbox-logo'>
+                        <input onChange={onTextChange} type='text' id='name' placeholder='Enter nickname' />
+                        <Link to='/ChatRoom'><button id='enterBtn' onClick={onEnterClick} style={{ visibility: 'hidden' }} /></Link>
+                    </div>
+                    <p id='nameAlert' style={{ visibility: 'hidden' }}>USERNAME HAS ALREADY BEEN TAKEN</p>
                 </form>
             </div>
         </div>
