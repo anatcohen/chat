582b62ddc9c69a3b42258412a66f23568bc68677
Anat <anatco13@gmail.com>
2020-03-25 15:53:01 +0200

    Updates

---
+++ precache-manifest.0d288698f45a1d56875134f32a0281a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5df51965c0a4f39e0f4ae17352daa72",
+    "url": "/ChatRoom/index.html"
+  },
+  {
+    "revision": "3c386178ce0bc6a7e3e5",
+    "url": "/ChatRoom/static/css/main.5d264413.chunk.css"
+  },
+  {
+    "revision": "99b0fc40ce498d950e4c",
+    "url": "/ChatRoom/static/js/2.5a546117.chunk.js"
+  },
+  {
+    "revision": "3c386178ce0bc6a7e3e5",
+    "url": "/ChatRoom/static/js/main.f74e3a1b.chunk.js"
+  },
+  {
+    "revision": "a5a432f48abb555c5587",
+    "url": "/ChatRoom/static/js/runtime-main.7e75b66a.js"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/ChatRoom/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "6b2049e3755170c75b03e465619bab08",
+    "url": "/ChatRoom/static/media/next.6b2049e3.svg"
+  },
+  {
+    "revision": "fd7ca6895a67f7c2713b4f13adeaecb5",
+    "url": "/ChatRoom/static/media/send.fd7ca689.png"
+  },
+  {
+    "revision": "956cf942cf66a5d7a1caa5fe0f128d73",
+    "url": "/ChatRoom/static/media/turn-off.956cf942.svg"
+  }
+]);
